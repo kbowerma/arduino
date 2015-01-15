14d523e4ca188c05dfe96710000ef5a9a0cf5a7b
Kyle Bowerman <kyle.bowerman@appirio.com>
2015-01-15 13:34:52 -0600

    added myBridge

---
+++ myBridge/myBridge.ino
@@ -0,0 +1,226 @@
+/*
+  Arduino Yún Bridge example
+
+ This example for the Arduino Yún shows how to use the
+ Bridge library to access the digital and analog pins
+ on the board through REST calls. It demonstrates how
+ you can create your own API when using REST style
+ calls through the browser.
+
+ Possible commands created in this shetch:
+
+ * "/arduino/digital/13"     -> digitalRead(13)
+ * "/arduino/digital/13/1"   -> digitalWrite(13, HIGH)
+ * "/arduino/analog/2/123"   -> analogWrite(2, 123)
+ * "/arduino/analog/2"       -> analogRead(2)
+ * "/arduino/mode/13/input"  -> pinMode(13, INPUT)
+ * "/arduino/mode/13/output" -> pinMode(13, OUTPUT)
+
+ This example code is part of the public domain
+
+ http://arduino.cc/en/Tutorial/Bridge
+
+Modified By Kyle Bowerman 1/6/2015
+
+ */
+
+#include <Bridge.h>
+#include <YunServer.h>
+#include <YunClient.h>
+#include <EEPROM.h>
+#include <Streaming.h>
+
+
+
+// Listen on default port 5555, the webserver on the Yún
+// will forward there all the HTTP requests for us.
+YunServer server;
+
+void setup() {
+  // Bridge startup
+  pinMode(13, OUTPUT);
+  digitalWrite(13, LOW);
+  Bridge.begin();
+  digitalWrite(13, HIGH);
+  // start serial port
+  Serial.begin(9600);
+  Serial.print("Bridge Demo");
+
+  // Listen for incoming connection only from localhost
+  // (no one from the external network could connect)
+  server.listenOnLocalhost();
+  server.begin();
+}
+
+// --------- functions ---------
+void printEEPROM(){
+  Serial.print(" printing the EEPROM");
+  for(int x = 0; x < 512; x++){
+    Serial.print(x);
+    Serial.print('\t');
+    Serial.print(EEPROM.read(x));
+    Serial.println();
+  }
+}
+
+
+//----------- loop ----------
+
+void loop() {
+  //Set up serial responder
+  if (Serial.available() > 0) {
+    int inByte = Serial.read();
+    switch(inByte) {
+      case 'i':
+       Serial << endl << "This is myBridge 1/6/2015" << endl;
+       break;
+      case 'e':
+       printEEPROM();
+       break;
+    }
+  }
+
+  // Get clients coming from server
+  YunClient client = server.accept();
+
+  // There is a new client?
+  if (client) {
+    // Process request
+    process(client);
+
+    // Close connection and free resources.
+    client.stop();
+  }
+
+  delay(50); // Poll every 50ms
+}
+
+void process(YunClient client) {
+  // read the command
+  String command = client.readStringUntil('/');
+
+  // is "digital" command?
+  if (command == "digital") {
+    digitalCommand(client);
+  }
+
+  // is "analog" command?
+  if (command == "analog") {
+    analogCommand(client);
+  }
+
+  // is "mode" command?
+  if (command == "mode") {
+    modeCommand(client);
+  }
+  if (command == "check") {
+    client << "this is test" << endl;
+    client << "Light A0: " << analogRead(0) << endl;
+    client << "Moisture A1: " << analogRead(1) << endl;
+  }
+}
+
+void digitalCommand(YunClient client) {
+  int pin, value;
+
+  // Read pin number
+  pin = client.parseInt();
+
+  // If the next character is a '/' it means we have an URL
+  // with a value like: "/digital/13/1"
+  if (client.read() == '/') {
+    value = client.parseInt();
+    digitalWrite(pin, value);
+  }
+  else {
+    value = digitalRead(pin);
+  }
+
+  // Send feedback to client
+  client.print(F("Pin D"));
+  client.print(pin);
+  client.print(F(" set to "));
+  client.println(value);
+
+  // Update datastore key with the current pin value
+  String key = "D";
+  key += pin;
+  Bridge.put(key, String(value));
+}
+
+void analogCommand(YunClient client) {
+  int pin, value;
+
+  // Read pin number
+  pin = client.parseInt();
+
+  // If the next character is a '/' it means we have an URL
+  // with a value like: "/analog/5/120"
+  if (client.read() == '/') {
+    // Read value and execute command
+    value = client.parseInt();
+    analogWrite(pin, value);
+
+    // Send feedback to client
+    client.print(F("Pin D"));
+    client.print(pin);
+    client.print(F(" set to analog "));
+    client.println(value);
+
+    // Update datastore key with the current pin value
+    String key = "D";
+    key += pin;
+    Bridge.put(key, String(value));
+  }
+  else {
+    // Read analog pin
+    value = analogRead(pin);
+
+    // Send feedback to client
+    client.print(F("Pin A"));
+    client.print(pin);
+    client.print(F(" reads analog "));
+    client.println(value);
+
+    // Update datastore key with the current pin value
+    String key = "A";
+    key += pin;
+    Bridge.put(key, String(value));
+  }
+}
+
+void modeCommand(YunClient client) {
+  int pin;
+
+  // Read pin number
+  pin = client.parseInt();
+
+  // If the next character is not a '/' we have a malformed URL
+  if (client.read() != '/') {
+    client.println(F("error"));
+    return;
+  }
+
+  String mode = client.readStringUntil('\r');
+
+  if (mode == "input") {
+    pinMode(pin, INPUT);
+    // Send feedback to client
+    client.print(F("Pin D"));
+    client.print(pin);
+    client.print(F(" configured as INPUT!"));
+    return;
+  }
+
+  if (mode == "output") {
+    pinMode(pin, OUTPUT);
+    // Send feedback to client
+    client.print(F("Pin D"));
+    client.print(pin);
+    client.print(F(" configured as OUTPUT!"));
+    return;
+  }
+
+  client.print(F("error: invalid mode "));
+  client.print(mode);
+}
